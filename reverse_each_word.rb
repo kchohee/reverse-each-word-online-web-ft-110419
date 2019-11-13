76697195d05275dccb66e7b5c329386ad1fa04ef
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 23:42:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,18 +3,6 @@ require 'pry'
 
 def reverse_each_word(string)
   string_parts = string.split(" ")
- # index = 0
- # wtf = []
- # while index < string_parts.size
- #  each_word = string_parts[index]
- #   index = index + 1
- #  wtf << each_word.reverse
- # end
- # string_parts.each do |each_word|
- # wtf << each_word.reverse
- # end
- # return wtf.join(" ")
-
  reversed_words_array = string_parts.collect do |word|
    word.reverse
  end
